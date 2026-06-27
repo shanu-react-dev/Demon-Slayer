8c13f3c162c8840fad788627b1a441584537b6fa
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-06-27 14:28:48 +0530

    array methods

--- JavaScript/Day43-Array and methods in JavaScript/script.js
+++ JavaScript/Day43-Array and methods in JavaScript/script.js
@@ -15,7 +15,7 @@
 // console.log(arr3);
 
 //! at
-let arrOfFruits = ["Grapes", "Orange"];
+// let arrOfFruits = ["Grapes", "Orange"];
 // let res = arrOfFruits.at(2);
 // res = arrOfFruits.at(-1);
 // console.log(res);
@@ -33,39 +33,107 @@ let arrOfFruits = ["Grapes", "Orange"];
 // console.log(res);
 
 //! flat
-console.log(arrOfFruits);
-let res = arrOfFruits.flat();
-console.log(res);
-let nestedArr = [
-  1,
-  2,
-  [3, 4, [5, ["Shanu", "Sundari", "Bandari", "Mundari"], 6], [7], 9],
-  [10, 11, 12],
-];
+// console.log(arrOfFruits);
+// let res = arrOfFruits.flat();
+// console.log(res);
+// let nestedArr = [
+//   1,
+//   2,
+//   [3, 4, [5, ["Shanu", "Sundari", "Bandari", "Mundari"], 6], [7], 9],
+//   [10, 11, 12],
+// ];
 // res = nestedArr.flat(2);
 // console.log(res);
 
-res = nestedArr.flat(Infinity);
-console.log(res);
-console.log(nestedArr);
+// res = nestedArr.flat(Infinity);
+// console.log(res);
+// console.log(nestedArr);
 
 //todo flatMap
 //! includes
-let arrOfUsers = ["Shashi", "Shanu", "Karthick", "Rashid", "Abrar"];
-res = arrOfUsers.includes("Selva");
-console.log(res);
+// let arrOfUsers = ["Shashi", "Shanu", "Karthick", "Rashid", "Abrar"];
+// res = arrOfUsers.includes("Selva");
+// console.log(res);
 
 //! indexOf
-res = arrOfUsers.indexOf("Shanu");
-console.log(res);
+// res = arrOfUsers.indexOf("Shanu");
+// console.log(res);
 
 //! join
-res = arrOfUsers.join("|");
-console.log(res);
-console.log(arrOfUsers);
+// res = arrOfUsers.join("|");
+// console.log(res);
+// console.log(arrOfUsers);
 
 //! keys
-res = arrOfUsers.keys();
-for (let i of res) {
-  console.log(i);
-}
+// res = arrOfUsers.keys();
+// for (let i of res) {
+//   console.log(i);
+// }
+
+//! lastIndexOf
+// let arr = ["Mercury", "venus", "Earth", "Mercury"];
+// let res = arr.lastIndexOf("Mercu");
+// console.log(res);
+
+// res = arr.length;
+// console.log(res);
+
+//! pop
+// console.log(arr);
+// res = arr.pop();
+// console.log(res);
+// console.log(arr);
+
+//!push
+let arr = ["Mercury", "venus", "Earth"];
+// let res = arr.push("Jupiter", "Uranus", "Neptune", "Mars", "Saturn");
+// console.log(res);
+// console.log(arr);
+
+//! shift
+// let res = arr.shift();
+// console.log(res);
+
+//! unshift
+// console.log(arr);
+// res = arr.unshift("Hii", "Byeeee");
+// console.log(res);
+// console.log(arr);
+
+// res = arr.concat(["Sundari", "Mundari", "Bandari"]);
+// console.log(res);
+// console.log(arr);
+
+// res = arr.push(["Sundari", "Mundari", "Bandari"]);
+// console.log(res);
+// console.log(arr);
+
+//!slice
+let arr2 = [10, 30, 20, 50, 100, 450];
+// console.log(arr2);
+// res = arr2.slice(3);
+// console.log(res);
+
+// res = arr2.slice(0, 4);
+// console.log(res);
+
+// res = arr2.slice(-5, -1);
+// console.log(res);
+
+//! splice
+console.log(arr2);
+// let res = arr2.splice(3);
+// console.log(res);
+// console.log(arr2);
+
+// let res = arr2.splice(3, 2);
+// console.log(res);
+// console.log(arr2);
+
+// let res = arr2.splice(3, 2, "Shanu");
+// console.log(res);
+// console.log(arr2);
+
+let res = arr2.splice(4, 0, "Nasa", true, "Rohit");
+console.log(res);
+console.log(arr2);
